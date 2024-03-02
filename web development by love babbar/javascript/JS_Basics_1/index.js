b82f3c3abb8af49fe631fc7b7976eb8b09dcaf9b
Suryawanshi Balkrishna Shivaji <116337185+BALKRISHNARF@users.noreply.github.com>
2024-03-02 22:13:59 +0530

    first commit

---
+++ web development by love babbar/javascript/JS_Basics_1/index.js
@@ -0,0 +1,71 @@
+//console.log(d)
+//cont num=12;
+//num =12;
+//let a=3;
+//let b=6;
+//let g=a+b*a;
+//console.log(g)
+
+let age=34;
+let status=(age>=18) ? 'I can vote ' :'cannot vote';
+console.log(status);
+
+
+console.log((true||'krishna'));
+console.log(('krishna'||true));
+console.log((false||'krishna'));
+console.log((true&&'krishna'));
+console.log(('krishna'&&true));
+console.log((false&&'krishna'));
+
+
+
+let marks =80;
+if(marks>=90){
+    console.log('A');
+}
+else if(marks>=80){
+    console.log('B');
+}
+else if (marks>=70){
+    console.log('C');
+}
+else if(marks>=60){
+    console.log('D');
+}
+else {
+    console.log('E');
+}
+
+let num=12;
+switch(num){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
+
+//for(let i=0;i<=5;i++){
+ //   console.log(i);
+//}
+
+
+
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+
+
+
+
+
+
+
+
+
